7ca7f4e7c37df4b71a7cceb81acbd471efe596e6
inhyuk15 <15inhyuk@gmail.com>
2023-07-30 21:28:23 +0900

    client 클래스 코드 분리
    main 함수에서 사용자 입력 받는 부분 분리
    io_context 작업 스레드 분리

--- testChatClient/main.cpp
+++ testChatClient/main.cpp
@@ -6,6 +6,64 @@ using boost::asio::ip::tcp;
 
 constexpr int BUFF_SIZE = 32;
 
+class Client {
+public:
+	Client(boost::asio::io_context& io_context, tcp::resolver::results_type& endpoints): io_context_(io_context), socket_(io_context) {
+		connect(endpoints);
+	}
+	
+	void close() {
+		boost::asio::post(io_context_,  [this](){ socket_.close();});
+	}
+	
+	void write(const char* writeBuff) {
+		boost::asio::async_write(socket_, boost::asio::buffer(writeBuff, std::strlen(writeBuff)), [this](boost::system::error_code ec, size_t byteTransferred) {
+			if (!ec) {
+				std::cout << "write sucesss, transferred : " << byteTransferred << "bytes" << std::endl;
+			} else {
+				std::cerr << "error in writing " << ec.message() << std::endl;
+				socket_.close();
+			}
+		});
+	}
+	
+	void read() {
+		boost::asio::async_read(socket_, boost::asio::buffer(readBuff_, 32), [this](boost::system::error_code ec, size_t bytesTransferred) {
+			if (!ec) {
+				std::cout << "reading " << std::endl;
+			}
+			else {
+				std::cerr << "error in reading" << std::endl;
+				socket_.close();
+			}
+		});
+	}
+	
+	void connect(tcp::resolver::results_type& endpoints) {
+		boost::asio::async_connect(socket_, endpoints, [this](boost::system::error_code ec, tcp::endpoint) {
+			if (!ec) {
+				std::cout << "connection success! " << std::endl;
+				connected_.store(true);
+				read();
+			} else {
+				std::cerr << "error in connection: " << ec.message() << std::endl;
+				socket_.close();
+			}
+		});
+	}
+	
+	bool isConnected() const {
+		return connected_.load();
+	}
+	
+private:
+	tcp::socket socket_;
+	boost::asio::io_context& io_context_;
+	char* readBuff_;
+	std::atomic<bool> connected_{false};
+};
+
+
 int main(int argc, const char * argv[]) {
 	try {
 		std::string port = "4000";
@@ -14,27 +72,25 @@ int main(int argc, const char * argv[]) {
 		boost::asio::io_context io_context;
 		tcp::resolver resolver(io_context);
 		auto endpoints = resolver.resolve(host, port);
+		Client client(io_context, endpoints);
 		
-		tcp::socket socket(io_context);
-		boost::asio::async_connect(socket, endpoints, [&socket](boost::system::error_code ec, tcp::endpoint) {
-			if (!ec) {
-				std::cout << "connection success! " << std::endl;
-				std::cout << "write msg ";
-				char writeBuff[BUFF_SIZE];
-				std::cin.getline(writeBuff, BUFF_SIZE);
-				boost::asio::async_write(socket, boost::asio::buffer(writeBuff, std::strlen(writeBuff)), [](boost::system::error_code ec, size_t byteTransferred) {
-					if (!ec) {
-						std::cout << "write sucesss, transferred : " << byteTransferred << "bytes" << std::endl;
-					} else {
-						std::cerr << "error in writing " << ec.message() << std::endl;
-					}
-				});
-			} else {
-				std::cerr << "error in connection: " << ec.message() << std::endl;
-			}
-		});
+		std::thread t([&io_context]() {io_context.run();});
 		
-		io_context.run();
+		while(!client.isConnected()) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		
+		while(true) {
+			std::cout << "write msg ";
+			char writeBuff[BUFF_SIZE];
+			std::cin.getline(writeBuff, BUFF_SIZE);
+			std::strcat(writeBuff, "\n");
+			client.write(writeBuff);
+		}
+		
+		client.close();
+		t.join();
+		
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
